66711bc8475aed1f0f7bb0097f3c51d482baa1ef
DenisdS <denis_santos1989@hotmail.com>
2018-11-03 13:31:20 -0300

    Style Header

--- src/scripts/controllers/main.controller.js
+++ src/scripts/controllers/main.controller.js
@@ -7,6 +7,21 @@ angular.module('app')
 			var checkRepos = 0;
 			var detailsUserGithub = "https://api.github.com/users/" + self + "?client_id=699079e80657c92a8f2a&client_secret=11b16c7bcf4ac50abc5635cebcb1675f011917bb";
 
+			$http.get(detailsUserGithub)
+				.then(
+					function(response){
+						$scope.login = response.data.login;
+						$scope.followers = response.data.followers;
+						$scope.following = response.data.following;
+						$scope.avatar_url = response.data.avatar_url;
+						$scope.email = response.data.email;
+						$scope.bio = response.data.bio;
+					},
+					function(err){
+						console.log("não encontrado!");
+					}
+				);
+
 			$http.get(url)
 				.then(
 					function(response){
@@ -22,18 +37,6 @@ angular.module('app')
 						$scope.incorrectReturn = 'Username não encontrado!';
 						$('ul').hide();
 						$('.errorSearch').show();
-					}
-				);
-
-				$http.get(detailsUserGithub)
-				.then(
-					function(response){
-						$scope.login = response.data.login;
-						$scope.followers = response.data.followers;
-						$scope.following = response.data.following;
-						$scope.avatar_url = response.data.avatar_url;
-						$scope.email = response.data.email;
-						$scope.bio = response.data.bio;
 					}
 				);
 		}
